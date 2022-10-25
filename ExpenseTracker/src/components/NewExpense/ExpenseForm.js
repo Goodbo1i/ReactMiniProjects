14d92b4b1539f9ae16b586002c56ee3a101eca61
Goodbo1i <63382553+Goodbo1i@users.noreply.github.com>
2022-10-25 15:01:31 +0300

    0.0.59

--- ExpenseTracker/src/components/NewExpense/ExpenseForm.js
+++ ExpenseTracker/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //   const [enteredTitle, setEnteredTitle] = useState("");
   //   const [enteredAmount, setEnteredAmount] = useState("");
   //   const [enteredDate, setEnteredDate] = useState("");
@@ -22,7 +22,7 @@ const ExpenseForm = () => {
       date: new Date(userInput.enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setUserInput(() => {
       return {
         enteredTitle: "",
