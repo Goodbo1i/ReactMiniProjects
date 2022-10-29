1a9d270f59318c0a6136088c20e7ac974a05fbd3
Goodbo1i <63382553+Goodbo1i@users.noreply.github.com>
2022-10-29 03:41:26 +0300

    Finished Tracker
    Tracker doesn't store to database.
    What can you do in this project:
    1. Add new Expense
    2. Filter Expenses by date
    3. See Chart of Expenses of chosen year
    4. Cancel Edited Expense

--- ExpenseTracker/src/components/NewExpense/NewExpense.js
+++ ExpenseTracker/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
+
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+
+  const [addIsActive, setAddIsActive] = useState(false)
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData)
+    activeHandler()
   };
+
+  const activeHandler = () => {
+    // 👇️ passed function to setState
+    setAddIsActive(current => !current);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {addIsActive && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={activeHandler}/>}
+      {!addIsActive && <button onClick={activeHandler}>Add New Expense</button>}
     </div>
-  );
+    )
+  
 };
 
 export default NewExpense;
